3c2fecd1a6c21356767f91e6620b42b9fddd07c8
masoud <mosibatzadeh@yahoo.com>
2018-04-03 00:57:01 +0430

    python added

--- README.md
+++ README.md
@@ -2,14 +2,32 @@
 
 This bash script automatically:
 
-* Installs LTS node.js via NVM (node version manager).
+* Installs LTS node.js via [nvm](https://github.com/creationix/nvm) (node version manager).
 
-* Installs latest stable npm (node.js package manager).
+* Installs latest stable [npm](https://www.npmjs.com) (node.js package manager).
 
-* Installs latest stable pm2 (used for running node.js applications as daemon)
+* Installs latest stable [pm2](http://pm2.keymetrics.io) (used for running node.js applications as daemon).
 
 ## Usage
 
 ```bash
 curl -o- https://raw.githubusercontent.com/Xaqron/scripts/master/install-node.sh | bash
+```
+
+# install-python.sh
+
+This bash script automatically:
+
+* Installs LTS python via [pyenv](https://github.com/pyenv/pyenv-installer) (python version manager).
+
+* Installs latest stable [pip](https://pypi.python.org/pypi) (python package manager).
+
+* Installs latest stable [setuptools](https://pypi.python.org/pypi/setuptools).
+
+* Installs latest stable [virtualenv](https://docs.python.org/3/library/venv.html).
+
+## Usage
+
+```bash
+curl -o- https://raw.githubusercontent.com/Xaqron/scripts/master/install-python.sh | bash
 ```
---
+++ install-python.sh
@@ -0,0 +1,62 @@
+echo "Installing prerequistics..."
+echo
+
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
+echo
+echo "Installing python version manager (pyenv)..."
+echo
+
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+# .bashrc content
+echo >> ~/.bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+echo
+echo "Installing LTS python..."
+echo
+
+echo "DEBUG: pyenv version= $(pyenv --version)"
+
+pyenv update
+LTS=$(pyenv install -l | grep '[[:space:]][[:digit:]].[[:digit:]].[[:digit:]]$' | tail -1)
+pyenv install $LTS
+pyenv global $LTS
+
+echo $(python -V) successfully installed in \"$(which python)\"
+echo
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install --upgrade virtualenv
+
+# pip install pylint
+
+if [ ! -z $(command -v code) ]; then
+   code --install-extension ms-python.python
+fi
+
+source ~/.bashrc
+
+# TODO: autoconfigure extension
+# path: ~/.config/Code/User/settings.json
+
+echo
+echo "To configure vscode:"
+echo "- Edit File > Preferences > Settings."
+echo "- Set vscode \"python.pythonPath\" to output of \"which python\" command."
+echo "- Set vscode \"python.venvPath\" to \"/home/user/.pyenv/versions/v.v.v/envs\"."
+echo "* For a new project in vscode edit \"launch.json\" and set \"stopOnEntry\": false"
+echo
+echo "Rebooting in 10 seconds..."
+sleep 10
+sudo reboot
